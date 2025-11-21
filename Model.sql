d1b7b7c6236465334bc14146311d7575a4e06ba2
lenjaKrah <s_krah23@stud.hwr-berlin.de>
2025-11-21 14:35:22 +0100

    Fehler mit Foreign Key anpassen

--- Model.sql
+++ Model.sql
@@ -1,13 +1,15 @@
--- Tabelle für Points of Interest (POIs)
-CREATE TABLE poi (
-    point_ID SERIAL FOREIGN KEY,
-    descriptionGER TEXT,
-    descriptionENG TEXT
-);
-
+-- Tabelle für Points 
 CREATE TABLE points ( 
    point_ID SERIAL PRIMARY KEY, 
    name VARCHAR(100) NOT NULL UNIQUE,
    x DOUBLE PRECISION NOT NULL,  -- X-Koordinate des Knotens
    y DOUBLE PRECISION NOT NULL,  -- Y-Koordinate des Knotens
 ); 
+
+-- Tabelle für Points of Interest (POIs)
+CREATE TABLE poi (
+    point_ID INTEGER PRIMARY KEY REFERENCES points(point_ID),
+    descriptionGER TEXT,
+    descriptionENG TEXT
+);
+
