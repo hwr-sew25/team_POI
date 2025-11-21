3ec369a492d00bbf07e48910467090696f4329b2
lenjaKrah <s_krah23@stud.hwr-berlin.de>
2025-11-21 14:28:45 +0100

    Points Table erstellen

--- Model.sql
+++ Model.sql
@@ -1,7 +1,13 @@
 -- Tabelle für Points of Interest (POIs)
 CREATE TABLE poi (
-    point_ID PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE,
-    beschreibungDeu TEXT,
-    beschreibungEng TEXT
+    point_ID SERIAL FOREIGN KEY,
+    descriptionGER TEXT,
+    descriptionENG TEXT
 );
+
+CREATE TABLE points ( 
+   point_ID SERIAL PRIMARY KEY, 
+   name VARCHAR(100) NOT NULL UNIQUE,
+   x DOUBLE PRECISION NOT NULL,  -- X-Koordinate des Knotens
+   y DOUBLE PRECISION NOT NULL,  -- Y-Koordinate des Knotens
+); 
