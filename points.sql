8c8c8bd16d8a6492f5cc44ec424c5929c34dd957
aesen <s_esenalieva23@stud.hwr-berlin>
2025-12-02 13:26:06 +0100

    West und Ostflügel als Point

---
+++ points.sql
@@ -0,0 +1,4 @@
+-- Alle Points, d.h. die Points of Interest und West- und Ostflügel
+-- Koordinaten werden später korrekt eingefügt
+INSERT INTO points (point_id, name, x, y) VALUES (1, 'Ost Fluegel', 0.0, 0.0);
+INSERT INTO points (point_id, name, x, y) VALUES (2, 'West Fluegel', 0.0, 0.0);
