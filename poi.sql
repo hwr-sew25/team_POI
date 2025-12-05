b783623d0893f5bc44a1f53c4045f6201b7f5427
aesen <s_esenalieva23@stud.hwr-berlin>
2025-12-05 10:07:54 +0100

    Points of Interest mit Inhalt

---
+++ poi.sql
@@ -0,0 +1,48 @@
+-- Bildungsangebote
+INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
+    3, 
+    'Hier finden Sie Flyer zu Nachhilfe, Schreibberatung und Workshops. Schauen Sie sich gerne um vielleicht ist etwas Passendes für Sie dabei.', 
+    'Here you can find flyers about tutoring, writing support, and workshops. Feel free to take a look - there might be something suitable for you.'
+);
+
+-- Spendenbox
+INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
+    4, 
+    'Hier können Sie mit Hygieneartikeln obdachlosen Menschen helfen. Spenden Sie verpackte Binden, Tampons oder Pflegeprodukte. Mehr Infos unter Social Period.', 
+    'Here you can help people experiencing homelessness by donating hygiene products. Please give sealed pads, tampons, or care items. Learn more at Social Period.'
+);
+
+-- Karten Validierer
+INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
+    5, 
+    'Sie sind nun beim Karten Validierer. Hier aktualisieren Sie die Gültigkeit Ihrer Studierendenkarte. Bitte schieben Sie dazu Ihre Karte vollständig mit dem Datum nach oben ein und warten Sie, bis der Vorgang abgeschlossen ist.', 
+    'You are now at the card validator. Here, you can update the validity of your student card. Please insert your card completely with the date facing upward and wait until the process is completed.'
+);
+
+--Kartenausgabe Gerät
+INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
+    6, 
+    'Sie stehen nun vor dem Kartenausgabe Gerät. Hier können Sie ihre Studierendenkarte erstellen. Drücken Sie dafür einfach den grünen Knopf ''Create Card'' auf dem Bildschirm.', 
+    'You are now standing in front of the card issuing machine. Here, you can create your student card. Simply press the green ''Create Card'' button on the screen.'
+);
+
+--Mülleimer
+INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
+    7, 
+    'Hier befindet sich der Mülleimer. Bitte trennen Sie Ihren Müll richtig.', 
+    'Here you can find the waste bin. Please make sure to separate your waste correctly.'
+);
+
+-- Kaffeeautomat
+INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
+    8, 
+    'Am Kaffeeautomaten können Sie aus verschiedenen kaffeehaltigen Getränken wählen. Die Bezahlung erfolgt ausschließlich mit Karte. Tippen Sie einfach auf Ihr Wunschgetränk, bezahlen Sie - und schon wird es frisch für Sie zubereitet.', 
+    'At the coffee machine, you can choose from a variety of coffee beverages. Payment is by card only. Simply tap your desired drink, make the payment, and it will be freshly prepared for you right away.'
+);
+
+-- Snackautomat
+INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
+    9, 
+    'Hier finden Sie Snacks und Getränke für eine kleine Pause. Der Automat akzeptiert sowohl Karte als auch Bargeld. Wenn möglich, wählen Sie gerne regionale oder nachhaltige Produkte.', 
+    'Here you can find snacks and drinks for a short break. The machine accepts both card and cash. Whenever possible, please choose regional or sustainable products.'
+);
