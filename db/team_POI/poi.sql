5bbbb5cb6fc30c9f4496068c5aa537ec3b153203
Ailine <ailine@example.com>
2026-01-30 11:05:32 +0100

    Deutsche Texte Umlaute entfernt

--- db/team_POI/poi.sql
+++ db/team_POI/poi.sql
@@ -1,76 +1,76 @@
 -- Bildungsangebote
 INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
     3, 
-    'Hier finden Sie Flyer zu Nachhilfe, Schreibberatung und Workshops. Schauen Sie sich gerne um vielleicht ist etwas Passendes für Sie dabei.', 
+    'Hier finden Sie Flyer zu Nachhilfe, Schreibberatung und Workshops. Schauen Sie sich gerne um vielleicht ist etwas Passendes fuer Sie dabei.', 
     'Here you can find flyers about tutoring, writing support, and workshops. Feel free to take a look - there might be something suitable for you.'
 );
 
 -- Spendenbox
 INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
     4, 
-    'Hier können Sie mit Hygieneartikeln obdachlosen Menschen helfen. Spenden Sie verpackte Binden, Tampons oder Pflegeprodukte. Mehr Infos unter Social Period.', 
+    'Hier koennen Sie mit Hygieneartikeln obdachlosen Menschen helfen. Spenden Sie verpackte Binden, Tampons oder Pflegeprodukte. Mehr Infos unter Social Period.', 
     'Here you can help people experiencing homelessness by donating hygiene products. Please give sealed pads, tampons, or care items. Learn more at Social Period.'
 );
 
 -- Karten Validierer
 INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
     5, 
-    'Sie sind nun beim Karten Validierer. Hier aktualisieren Sie die Gültigkeit Ihrer Studierendenkarte. Bitte schieben Sie dazu Ihre Karte vollständig mit dem Datum nach oben ein und warten Sie, bis der Vorgang abgeschlossen ist.', 
+    'Sie sind nun beim Karten Validierer. Hier aktualisieren Sie die Gueltigkeit Ihrer Studierendenkarte. Bitte schieben Sie dazu Ihre Karte vollstaendig mit dem Datum nach oben ein und warten Sie, bis der Vorgang abgeschlossen ist.', 
     'You are now at the card validator. Here, you can update the validity of your student card. Please insert your card completely with the date facing upward and wait until the process is completed.'
 );
 
 --Kartenausgabe Gerät
 INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
     6, 
-    'Sie stehen nun vor dem Kartenausgabe Gerät. Hier können Sie ihre Studierendenkarte erstellen. Drücken Sie dafür einfach den grünen Knopf ''Create Card'' auf dem Bildschirm.', 
+    'Sie stehen nun vor dem Kartenausgabe Geraet. Hier koennen Sie ihre Studierendenkarte erstellen. Druecken Sie dafuer einfach den gruenen Knopf ''Create Card'' auf dem Bildschirm.', 
     'You are now standing in front of the card issuing machine. Here, you can create your student card. Simply press the green ''Create Card'' button on the screen.'
 );
 
 --Mülleimer
 INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
     7, 
-    'Hier befindet sich der Mülleimer. Bitte trennen Sie Ihren Müll richtig.', 
+    'Hier befindet sich der Muelleimer. Bitte trennen Sie Ihren Muell richtig.', 
     'Here you can find the waste bin. Please make sure to separate your waste correctly.'
 );
 
 -- Kaffeeautomat
 INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
     8, 
-    'Am Kaffeeautomaten können Sie aus verschiedenen kaffeehaltigen Getränken wählen. Die Bezahlung erfolgt ausschließlich mit Karte. Tippen Sie einfach auf Ihr Wunschgetränk, bezahlen Sie - und schon wird es frisch für Sie zubereitet.', 
+    'Am Kaffeeautomaten koennen Sie aus verschiedenen kaffeehaltigen Getraenken waehlen. Die Bezahlung erfolgt ausschließlich mit Karte. Tippen Sie einfach auf Ihr Wunschgetraenk, bezahlen Sie - und schon wird es frisch fuer Sie zubereitet.', 
     'At the coffee machine, you can choose from a variety of coffee beverages. Payment is by card only. Simply tap your desired drink, make the payment, and it will be freshly prepared for you right away.'
 );
 
 -- Snackautomat
 INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
     9, 
-    'Hier finden Sie Snacks und Getränke für eine kleine Pause. Der Automat akzeptiert sowohl Karte als auch Bargeld. Wenn möglich, wählen Sie gerne regionale oder nachhaltige Produkte.', 
+    'Hier finden Sie Snacks und Getraenke für eine kleine Pause. Der Automat akzeptiert sowohl Karte als auch Bargeld. Wenn moeglich, waehlen Sie gerne regionale oder nachhaltige Produkte.', 
     'Here you can find snacks and drinks for a short break. The machine accepts both card and cash. Whenever possible, please choose regional or sustainable products.'
 );
 
 -- HWR Geschichte
 INSERT INTO poi (poi_id, descriptionGER, descriptionENG) VALUES (
     10,
-    'Der Campus Schöneberg der HWR hat einen besonderen historischen
+    'Der Campus Schoeneberg der HWR hat einen besonderen historischen
     Hintergrund, da das Haus B bereits 1939 erbaut wurde. Von 1940 bis Kriegsende
-    nutzte das Oberkommando der Wehrmacht das Hause für die Erstellung
+    nutzte das Oberkommando der Wehrmacht das Hause fuer die Erstellung
     rassistischer Regelungen für Kriegsgefangenenlager.
     Nach dem Wiederaufbau ab 1951 zog die Deutsche Hochschule für Politik ein, die
-    später in die FU Berlin eingegliedert wurde.
-    Ab 1971 entstand in dem Gebäude die Fachhochschule für Wirtschaft (FHW), eine
+    spaeter in die FU Berlin eingegliedert wurde.
+    Ab 1971 entstand in dem Gebaeude die Fachhochschule fuer Wirtschaft (FHW), eine
     Reformhochschule mit Fokus auf praxisorientierter Betriebswirtschaftslehre.
-    Seit 2009 bildet das Gebäude nach einer Fusion einen Hauptstandort der neu
-    gegründeten Hochschule für Wirtschaft und Recht Berlin (HWR Berlin). Somit
+    Seit 2009 bildet das Gebaeude nach einer Fusion einen Hauptstandort der neu
+    gegruendeten Hochschule fuer Wirtschaft und Recht Berlin (HWR Berlin). Somit
     spiegelt der Campus die wechselvolle Geschichte Berlins und die Entwicklung der
     wirtschaftswissenschaftlichen Ausbildung wider.
-    Mehr Informationen können Sie auf der HWR Webseite finden: https://www.hwrberlin.de/hwr-berlin/ueber-uns/geschichte',
+    Mehr Informationen koennen Sie auf der HWR Webseite finden: https://www.hwrberlin.de/hwr-berlin/ueber-uns/geschichte',
 
-    'The Schöneberg campus of the Berlin School of Economics and Law (HWR
+    'The Schoeneberg campus of the Berlin School of Economics and Law (HWR
 Berlin) has a unique historical background, as Building B was constructed as early as
 1939. From 1940 until the end of World War II, the High Command of the German
 Armed Forces (Oberkommando der Wehrmacht) used the building to draft racist
 regulations for prisoner-of-war camps.
 After reconstruction began in 1951, the German University of Politics (Deutsche
-Hochschule für Politik) moved in, which was later integrated into Freie Universität
+Hochschule für Politik) moved in, which was later integrated into Freie Universitaet
 Berlin.
 In 1971, the building became home to the University of Applied Sciences for
 Economics (Fachhochschule für Wirtschaft – FHW), a reform university focusing on
